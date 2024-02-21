ff8d630550d2089d5e425102e2cfbfb194e19f1d
suryarajrs <suryars@outlook.in>
2024-02-21 19:00:56 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
+
+    const [id , setid]=useState(0);
+    const [data , setdata]=useState("");
+    const [loading , setloading] = useState(false);
+
+
+  const handleOnchange = async (e)=>{
+    
+    const id = e.target.value;
+    if(id){
+        setloading(true);
+    }
+    try{
+
+        const responce  =  await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
+    
+        const results = await responce.json();
+        setdata(results)
+        setloading(false)
+    }catch(error){
+        console.log(error)
+    }
+  }
+  
+    return(
+         <>
+         
+         <label htmlFor='id'>ID number</label>
+         <input id='id' onChange={handleOnchange} type="number" />
+
+         {loading&& <Loader></Loader> }
+         {data&& <PhotoFrame data = {data}></PhotoFrame>}
+         
+         </>
+    )
   
 }
 
--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-export const PhotoFrame = () => {
+export const PhotoFrame = (props) => {
+   {console.log(props.data.url)}
+
+    return (
+
+        <>
+        
+        <div className='photoframe'>
+         <img src={props.data.url}/>
+
+        </div>
+        <div className='caption'>
+            <h5>{props.data.title}</h5>
+        </div>
+        
+        </>
+    )
    
 }
